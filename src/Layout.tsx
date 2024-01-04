90ddb12283d12a18ea5457bc548965a44fb6610b
MT <04x0xx@gmail.com>
2024-01-04 05:57:04 -0400

    Added First Sections of Code

---
+++ src/Layout.tsx
@@ -0,0 +1,11 @@
+import "./Layout.css";
+
+function Layout({ children }) {
+	return (
+		<div className="Layout">
+			{children}
+		</div>
+	);
+}
+
+export default Layout;
---
+++ src/Sidebar.tsx
@@ -0,0 +1,10 @@
+function Sidebar({ children }) {
+	// .Sidebar css found in Layout.css
+	return (
+		<div className="Sidebar">
+			{children}
+		</div>
+	);	
+}
+
+export default Sidebar;
