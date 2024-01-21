aa4171301330a1b23a5ab794991e23133f18a379
MT <04x0xx@gmail.com>
2024-01-21 00:22:28 -0400

    Added all bindings to home UI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,18 +4,36 @@
 use std::process::Command;
 
 #[tauri::command]
-fn kyros() -> String {
+fn kyros(
+        julia: bool,
+        measurement: &str,
+        formula: &str,
+        resolution: &str,
+        _rate_of_color_change: &str,
+        max_i: &str,
+        color_style: &str,
+    ) -> String {
 
+    // Initializes Command Arguments
+    let mut command_args = vec![
+        "-p", resolution,
+        "-i", max_i,
+        "-f", formula,
+        "--color", color_style,
+        "--save-method", "B64",
+        "-y"
+    ];
+
+    if julia { command_args.push("-j") }
+    if measurement == "TD" { command_args.push("--travel-distance") }
+
+    // Sets and calls executable
     let filename = "./bins/kyros.exe";
-
-    let output = Command::new(filename)
-        .args([
-              "--save-method", "B64",
-              "-y"
-        ])
+    let child = Command::new(filename)
+        .args(command_args)
         .output();
 
-    return match output {
+    return match child {
         Ok(image) => format!("data:image/png;base64,{}", String::from_utf8(image.stdout).unwrap()),
         Err(error) => format!("Threw Error: {:?}", error),
     }
