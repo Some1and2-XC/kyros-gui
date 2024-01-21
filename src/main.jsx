ecc2fbbe4fe0b41050b10a97b93e864eb311f1eb
MT <04x0xx@gmail.com>
2024-01-21 00:31:39 -0400

    Prepared project for build

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
 		<App />
     </React.StrictMode>,
